3e8c6a1cc61c9bf7f303191476f0662e2989629a
Jula000 <128170039+Jula000@users.noreply.github.com>
2024-06-19 22:28:58 -0500

    Fix hero page & create about page
    lublu ev

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,11 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Manrope } from "next/font/google";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const manrope = Manrope({ subsets: ["latin"] });
+const kek = "test";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} ${kek} ${manrope.className}`}>
+        {children}
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,15 @@ import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Header from "./components/Header";
 import PhotoGrid from "./components/PhotoGrid";
+import About from "./components/AboutPage.jsx";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col bg-main-color">
+    <main className={`flex min-h-screen flex-col bg-main-color`}>
       <Header />
       <HeroSection />
       <PhotoGrid />
+      <About />
     </main>
   );
 }
